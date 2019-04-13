af05b064ecb1e50ac35de85537bec673f4583948
Al Masgai <alex.masgai@gmail.com>
2019-04-13 00:08:14 -0400

    Fixed some changes

--- main.R
+++ main.R
@@ -7,40 +7,38 @@ pc <- 0.75
 a <- 0.5
 b <- 0.5
 
+x1 <- runif(1, 10, 80)
+x2 <- runif(1, 10, 50)
+x3 <- runif(1, 0.9, 5.0)
+x4 <- runif(1, 0.9, 5.0)
+
 # Initializing functions f1 & f2. f3 is the minimum between f1 & f2
-f1 <- function(x1, x2, x3, x4) { 2 * x2 * x4 + x3 * (x1 - 2 * x4) }
-f2 <- function(x1, x2, x3, x4) { 60000 / (x3 * ((x1 - 2 * x4) ** 3) + 2 * x2 * x4 * ((4 * x4 ** 2) + 3 * x1 * (x1 - 2 * x4))) }
-f3 <- function(f1, f2) { a * f1 + b * f2 }
+f1 <- function(x1 = runif(1, 10, 80), x2 = runif(1, 10, 50), x3 = runif(1, 0.9, 5.0), x4 = runif(1, 0.9, 5.0)) { 
+  2 * x2 * x4 + x3 * (x1 - 2 * x4) 
+}
+
+f2 <- function(x1 = runif(1, 10, 80), x2 = runif(1, 10, 50), x3 = runif(1, 0.9, 5.0), x4 = runif(1, 0.9, 5.0)) { 
+  60000 / (x3 * ((x1 - 2 * x4) ** 3) + 2 * x2 * x4 * ((4 * x4 ** 2) + 3 * x1 * (x1 - 2 * x4))) 
+}
+
+f3 <- function() { 
+  x1 <- runif(1, 10, 80)
+  x2 <- runif(1, 10, 50)
+  x3 <- runif(1, 0.9, 5.0)
+  x4 <- runif(1, 0.9, 5.0)
+  a * f1(x1, x2, x3, x4) + b * f2(x1, x2, x3, x4)
+}
 
 # The general optimization goal for the I-Beam problem is to minimize F=[f1,f2]T 
 # where the cross section area (f1) and the static deflection (f2) of the I-beam.
 
-GA1 <- ga(type="real-valued", fitness=f1, lower = c(10, 10, 0.9, 0.9), upper = c(80, 50, 4.0, 5.0),  pmutation = pm,  pcrossover = pc, popSize = populationSize)
-GA2 <- ga(type="real-valued", fitness=f2, lower = c(10, 10, 0.9, 0.9), upper = c(80, 50, 4.0, 5.0), pmutation = pm,  pcrossover = pc, popSize = populationSize)
-GA3 <- ga(type="real-valued", fitness=f3, lower = c(10, 10, 0.9, 0.9), upper = c(80, 50, 4.0, 5.0), pmutation = pm,  pcrossover = pc, popSize = populationSize)
+GA1 <- ga(type="real-valued", fitness=f1, lower = c(10, 10, 0.9, 0.9), upper = c(80, 50, 5.0, 5.0), pmutation = pm, pcrossover = pc, popSize = populationSize)
+GA2 <- ga(type="real-valued", fitness=f2, lower = c(10, 10, 0.9, 0.9), upper = c(80, 50, 5.0, 5.0), pmutation = pm, pcrossover = pc, popSize = populationSize)
+GA3 <- ga(type="real-valued", fitness=f3, lower = c(10, 10, 0.9, 0.9), upper = c(80, 50, 5.0, 5.0), pmutation = pm, pcrossover = pc, popSize = populationSize)
 
-plot(f1(x1, x2, x3, x4),
-     main = "Cross Section Area (f1)",
-     xlab = "Generation No.", 
-    ylab = "Best Particle Fitness")
-
-plot(f2(x1, x2, x3, x4),
-     main = "Static Delfection (f2)",
-     xlab = "Generation No.", 
-     ylab = "Best Particle Fitness")
-
-plot(f3(f1, f2),
-     main = "F = a * f1 + b * f2",
-     xlab = "Generation No.", 
-     ylab = "Best Particle Fitness")
-
-summary(x1, x2, x3, x4)
+plot(f1, main = "Cross Section Area (f1)", xlab = "Generation No.", ylab = "Best Particle Fitness")
+plot(f2, main = "Static Deflection (f2)", xlab = "Generation No.", ylab = "Best Particle Fitness")
+plot(f3, main = "F = a * f1 + b * f2", xlab = "Generation No.", ylab = "Best Particle Fitness")
 
 plot(GA1)
-summary(GA1)
-
 plot(GA2)
-summary(GA2)
-
-plot(GA3)
-summary(GA3)
